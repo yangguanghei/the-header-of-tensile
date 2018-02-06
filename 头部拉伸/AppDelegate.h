896b6a5383b785d8542821ed373f17d4cc9d005e
yangguanghei <1096359518@qq.com>
2018-02-06 11:17:02 +0800

    Initial Commit

---
+++ 头部拉伸/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  头部拉伸
+//
+//  Created by 梁森 on 2018/2/6.
+//  Copyright © 2018年 ----. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 头部拉伸/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  头部拉伸
+//
+//  Created by 梁森 on 2018/2/6.
+//  Copyright © 2018年 ----. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
